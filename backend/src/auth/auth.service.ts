34ba0d59945de7c072fd9ef6c5edb6ef452c230d
neosizzle <junhanngd1115@gmail.com>
2022-06-27 14:40:59 +0800

    split function

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -13,38 +13,62 @@ interface TokenInfo {
 	error_description? : string,
 }
 
+
 @Injectable()
 export class AuthService {
 	constructor(private prisma : PrismaService, private config : ConfigService){}
 
+	// TODO : TEST THIS
+	private async getTokenInfo(code : string) : Promise<TokenInfo> {
+		const payload = new FormData();
+
+		payload.append('grant_type', 'authorization_code');
+		payload.append('client_id', this.config.get("API_UID_42"));
+		payload.append('client_secret', this.config.get("API_SECRET_42"));
+		payload.append('code', code);
+		payload.append('redirect_uri', this.config.get('API_REDIR_URI_42'));
+
+		return fetch(`${this.config.get('API_HOST_42')}/oauth/token`, {
+			method : "POST",
+			body : payload
+		}).then(res => res.json())
+	}
+
 	async authenticate(code : string)
 	{
 		let	tokenInfo : TokenInfo;
+		let	userInfo : any;
 
 		// fetch token with code
 		try {
-			const payload = new FormData();
-			payload.append('grant_type', 'authorization_code');
-			payload.append('client_id', this.config.get("API_UID_42"));
-			payload.append('client_secret', this.config.get("API_SECRET_42"));
-			payload.append('code', code);
-			payload.append('redirect_uri', this.config.get('API_REDIR_URI_42'));
-
-			const res = await fetch(`${this.config.get('API_HOST_42')}/oauth/token`, {
-				method : "POST",
-				body : payload
-			})
-
-			tokenInfo = await res.json();
+			tokenInfo = await this.getTokenInfo(code);
 			if (tokenInfo.error) throw new Error(JSON.stringify(tokenInfo))
 		} catch (error) {
+			// send error if code is invalid
+
 			console.error(error.message);
 			throw new UnauthorizedException(error.message);
 		}
 
 		// get 42 user info 
+		try {
+			const res = await fetch(`${this.config.get('API_HOST_42')}/oauth/token`, {
+				method : "GET",
+				headers: {
+					'Authorization': `Bearer ${tokenInfo.access_token}`
+					},
+			})
+
+			const userInfo = await res.json();
+			if (userInfo.error) throw new Error(JSON.stringify(userInfo))
+		} catch (error) {
+			// send error if request fails
+			console.error(error.message);
+			throw new UnauthorizedException(error.message);
+		}
 
 		// get or create user in db
+		
 
 		// calculate expire date token
 
