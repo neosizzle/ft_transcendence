1812c9f30bd29d592c1def14280aefd817118377
PandieYay <babyedison@gmail.com>
2022-07-03 13:29:46 +0800

    Chat W.I.P

--- frontend/src/Chat/index.tsx
+++ frontend/src/Chat/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import "./index.css";
 
-const Chat = () => {
+function Chat() {
+	let messages = "Test";
+
+	function myFunction(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		messages = "Boom";
+		alert("The form was submitted");
+	}
+
 	return (
+		<>
 		<div>
 			<h2>chat</h2>
 		</div>
+		{messages}
+		<form onSubmit={(myFunction)} className="Chat">
+			<input/>
+			<button type="submit">Submit</button>
+		</form>
+		</>
 	);
-};
+}
 
 export default Chat;
