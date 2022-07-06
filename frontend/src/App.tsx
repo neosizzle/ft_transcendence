dac4344548d49386cf1cacb91580695ab180bb02
PandieYay <babyedison@gmail.com>
2022-07-06 10:08:54 +0800

    Tailwindcss added, styled navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,36 +11,36 @@ import Game from './Game';
 import Chat from './Chat';
 
 function App() {
-  
   return (
     <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/chat">Chat</Link>
-              </li>
-              <li>
-                <Link to="/users">Users</Link>
-              </li>
-              <li>
-                <Link to="/game">Game</Link>
-              </li>
-            </ul>
-          </nav>
-
-          {/* A <Route> looks through its children <Route>s and
+      <div>
+        <nav className='bg-gray-800'>
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="flex-shrink-0 flex items-center">
+                {/* Img can be changed */}
+                <img className="block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow" />
+              </div>
+              <div className="sm:block sm:ml-6">
+                <div className="flex space-x-4">
+                  <Link to="/" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</Link>
+                  <Link to="/chat" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Chat</Link>
+                  <Link to="/users" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Users</Link>
+                  <Link to="/game" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Game</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+        {/* A <Route> looks through its children <Route>s and
               renders the first one that matches the current URL. */}
-          <Routes>
-            <Route path="/chat" element = {<Chat/>}/>
-            <Route path="/game" element = {<Game/>}/>
-            <Route path="/users" element = {<User/>}/>
-            <Route path="/" element = {<Home/>}/>
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/game" element={<Game />} />
+          <Route path="/users" element={<User />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
