2d71acc7612b888ea09db7c5dd94970e656ff5b0
Ng Wee Hean <heanconcerto@gmail.com>
2022-07-08 11:31:20 +0800

    Added walls onto game background

--- frontend/src/Game/pong.ts
+++ frontend/src/Game/pong.ts
@@ -2,6 +2,7 @@
 class Pong {
 	private canvas: HTMLCanvasElement;
 	private ctx: CanvasRenderingContext2D;
+	private wall: Wall[] = [];
 	
 	constructor() {
 		this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
@@ -10,22 +11,47 @@ class Pong {
 		this.create_background();
 	}
 	
-	// fill the background of canvas black
+	// create background for the game
 	create_background() {
+		// fill the background with black colour
 		this.ctx.fillStyle = "black";
 		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+		
+		// create walls
+		const width = this.canvas.width;
+		const height = this. canvas.height;
+		const border = this.canvas.width * 0.025;
+		
+		this.wall.push(new Wall(width/2, border/2, width, border));
+		this.wall.push(new Wall(width/2, height - border/2, width, border));
+		this.wall.push(new Wall(border/2, height/2, border, height));
+		for (let i = 0; i < this.wall.length; i++)
+			this.wall[i].draw(this.ctx)
 	}
 }
 
 
 class Wall {
-	static thickness: number;
+	static colour = "white";
 	
 	private x: number;
 	private y: number;
+	private width: number;
+	private height: number;
 	
-	draw() {
-		return ;
+	constructor(x: number, y: number, width: number, height: number) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+	
+	// draw the wall using the context
+	draw(ctx: CanvasRenderingContext2D): void {
+		ctx.fillStyle = Wall.colour;
+		ctx.fillRect(
+			this.x - this.width/2, this.y - this.height/2,
+			this.x + this.width/2, this.y + this.height/2);
 	}
 }
 
