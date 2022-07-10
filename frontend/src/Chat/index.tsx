40c1aaf8795b330681b9f3c8045aed01f435d1bf
PandieYay <babyedison@gmail.com>
2022-07-10 19:57:41 +0800

    Working local chat

--- frontend/src/Chat/index.tsx
+++ frontend/src/Chat/index.tsx
@@ -1,22 +1,36 @@
+import { useState } from 'react';
 import React from 'react';
 
 function Chat() {
-	let messages = "Test";
+	const [message, setMessage] = useState('');
 
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		console.log(setMessage);
+		setMessage(event.target.value);
+	};
+	const handleClick = () => {
+		// 👇️ clear input value
+		setMessage('');
+	};
 	function myFunction(event: React.FormEvent<HTMLFormElement>) {
 		event.preventDefault();
-		messages = "Boom";
-		alert("The form was submitted");
+		const element = document.getElementById("chat");
+		const message = (document.getElementById("message") as HTMLInputElement).value;
+		if (element && message) {
+			element.innerHTML += message;
+			element.innerHTML += '<br>';
+		}
+		handleClick();
 	}
 
 	return (
 		<>
 			<div>
-				<h1 className='text-5xl'>Chat</h1>
+				<h1 className='text-5xl' id="MyHeader">Chat</h1>
 			</div>
-			{messages}
+			<p id="chat" className='h-96'></p>
 			<form onSubmit={(myFunction)} className="Chat">
-				<input className="w-fit mt-10 border-2" placeholder="Say something..." />
+				<input type="text" className="w-fit mt-10 border-2" placeholder="Say something..." id="message" onChange={handleChange} value={message}/>
 			</form>
 		</>
 	);
