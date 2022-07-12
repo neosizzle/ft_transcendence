f967676528eaccf122ab83942ac3623864379573
PandieYay <babyedison@gmail.com>
2022-07-12 14:56:29 +0800

    movies added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesModule } from './movies/movies.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, MoviesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/movies/movies.module.ts
+++ backend/src/movies/movies.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { MoviesController } from './movies.controller';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [MoviesController]
+  controllers: [MoviesController],
+  providers: [PrismaService, ConfigService]
 })
 export class MoviesModule {}
