bd3f7a71b8f436508651d721f0f6cad10efdac88
PandieYay <babyedison@gmail.com>
2022-07-16 15:18:20 +0800

    Added user and chat models

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [PrismaModule, MoviesModule],
+  imports: [PrismaModule, MoviesModule, UserModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
