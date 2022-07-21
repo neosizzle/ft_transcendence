2d8eb69e88a1ed440226dab94208d526e8bf331a
PandieYay <babyedison@gmail.com>
2022-07-21 14:15:33 +0800

    Fixed Dto for chat and movies

--- backend/src/chat/chat.dto.ts
+++ backend/src/chat/chat.dto.ts
@@ -1,6 +1,17 @@
+import {IsDateString, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
 export class chatDto {
     id: number;
+
+    @IsNotEmpty()
+    @IsNumber()
     UserID: number;
+
+    @IsNotEmpty()
+    @IsDateString()
     timestamp: Date;
+
+    @IsNotEmpty()
+    @IsString()
     message: string;
 }
