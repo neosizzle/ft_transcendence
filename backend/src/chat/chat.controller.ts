236437c7763d20cc7ea2d0e763269de942ef3b56
PandieYay <babyedison@gmail.com>
2022-07-21 11:06:57 +0800

    Added GET and POST for chat

--- backend/src/chat/chat.controller.ts
+++ backend/src/chat/chat.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { chatDto } from './chat.dto';
 
 @Controller('chat')
-export class ChatController {}
+export class ChatController {
+    constructor(private readonly prismaService: PrismaService) {}
+
+    @Get()
+    getChat(): Promise<chatDto[]> {
+        return this.prismaService.chat.findMany();
+    }
+    @Post()
+    create(@Body() {UserID, timestamp, message} : chatDto): Promise<chatDto> {
+        return this.prismaService.chat.create({
+            data: {UserID, timestamp, message},
+        });
+    }
+}
