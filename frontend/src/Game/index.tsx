d6fd97a5c2df42148ac73fc754da93c8190306e1
Ng Wee Hean <heanconcerto@gmail.com>
2022-07-25 00:14:02 +0800

    Added simple animation to canvas

--- frontend/src/Game/index.tsx
+++ frontend/src/Game/index.tsx
@@ -3,6 +3,14 @@ import React, { useRef, useEffect } from 'react';
 const Game = (props: any) => {
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 	
+	const draw = (ctx: CanvasRenderingContext2D, frameCount: number) => {
+		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+		ctx.fillStyle = '#000000'
+		ctx.beginPath()
+		ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
+		ctx.fill()
+	}
+	
 	// Effect Hook performs side effect in function components
 	useEffect(() => {
 		const canvas = canvasRef.current;
@@ -12,7 +20,23 @@ const Game = (props: any) => {
 		const context = canvas.getContext('2d');
 		if (context == null)
 			return ;
-		}, []);
+		
+		let frameCount = 0;
+		let animationFrameId: number;
+		
+		const render = () => {
+			++frameCount;
+			draw(context, frameCount);
+			animationFrameId = window.requestAnimationFrame(render);
+		}
+		render();
+		
+		return () => {
+			// ensure animation frame is cancelled after canvas is unmounted
+			window.cancelAnimationFrame(animationFrameId)
+		}
+		
+		}, [draw]);
 	
 	return (
 		<canvas
