eaa5b95fbd642700c5cf641b112bb37f06286c96
Ng Wee Hean <heanconcerto@gmail.com>
2022-07-24 23:55:02 +0800

    Added an empty canvas

--- frontend/src/Game/index.tsx
+++ frontend/src/Game/index.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
-const Game = () => {
+const Game = (props: any) => {
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+	
+	// Effect Hook performs side effect in function components
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		if (canvas == null)
+			return ;
+		
+		const context = canvas.getContext('2d');
+		if (context == null)
+			return ;
+		}, []);
+	
 	return (
-		<div>
-			<h2>game</h2>
-		</div>
-	);
-};
+		<canvas
+			ref={canvasRef}
+			width="400"
+			height="300"
+			style={{border: "1px solid black"}}
+			{...props}
+		/>
+	)
+}
 
 export default Game;
