e21bdd07a2a7f6fbaef07a15e88aed9579b6def2
Ng Wee Hean <heanconcerto@gmail.com>
2022-07-27 17:36:13 +0800

    Established connection between server and client via socket.io

--- frontend/src/Game/index.tsx
+++ frontend/src/Game/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import io from 'socket.io-client';
+
 import { Canvas } from './Canvas';
 
 
 class Game extends React.Component {
 	render() {
+		const socket = io("ws://localhost:4896"); 
+		
+		socket.on("hello from server", (...args) => {
+			console.log("Connected to server.");
+		});
+		
 		return <Canvas width={400} height={300} style={{border: "1px solid black"}}/>;
 	}
 }
