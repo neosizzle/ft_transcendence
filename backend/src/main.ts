5db5935525b17448bb7474028fd4be155db4f332
Ng Wee Hean <heanconcerto@gmail.com>
2022-07-28 15:05:31 +0800

    Implemented server-side socket connection using Gateways

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { createServer } from "http";
-import { Server } from 'socket.io';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,22 +10,6 @@ async function bootstrap() {
     whitelist: true
   }));
   await app.listen(3001);
-  
-  // create a server that listens to a different port
-  const httpServer = createServer();
-  const io = new Server(httpServer, {
-    // need to handle CORS: https://socket.io/docs/v4/handling-cors/
-    cors: {
-      origin: 'http://localhost:3000',	// need to be changed in production
-    }
-  });
-  httpServer.listen(4896);
-  
-  io.on('connection', (socket) => {
-    console.log('a user connected');
-    
-    socket.emit("hello from server");
-  });
-  
 }
+
 bootstrap();
--- frontend/src/Game/index.tsx
+++ frontend/src/Game/index.tsx
@@ -6,10 +6,28 @@ import { Canvas } from './Canvas';
 
 class Game extends React.Component {
 	render() {
-		const socket = io("ws://localhost:4896"); 
+		const socket = io("http://localhost:3001");  // change for production
 		
-		socket.on("hello from server", (...args) => {
-			console.log("Connected to server.");
+		// 'connect' event is fired upon connection the the Namespace
+		socket.on('connect', function() {
+			console.log('Connected');
+	
+			socket.emit('events', { test: 'test' });
+			socket.emit('identity', 0, (response: any) =>
+				console.log('Identity:', response),
+			);
+		});
+		
+		socket.on('events', function(data) {
+			console.log('event', data);
+		});
+		
+		socket.on('exception', function(data) {
+			console.log('event', data);
+		});
+		
+		socket.on('disconnect', function() {
+			console.log('Disconnected');
 		});
 		
 		return <Canvas width={400} height={300} style={{border: "1px solid black"}}/>;
