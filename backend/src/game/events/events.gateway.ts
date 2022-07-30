a955ddc979cbd54443e318832cb02a67e78192d1
Ng Wee Hean <heanconcerto@gmail.com>
2022-07-30 16:25:56 +0800

    Server only records keyup and keydown events from game players

--- backend/src/game/events/events.gateway.ts
+++ backend/src/game/events/events.gateway.ts
@@ -22,59 +22,66 @@ export class GameEventsGateway
 	@WebSocketServer()
 	server: Server;
 	keypress: KeyPressMonitor = KeyPressMonitor.get_instance();
-	connections: Map<string, number> = new Map();
-	
-	private player = -1;
-	private valid_key: Map<number, string[]> = new Map([
-		[0, ["Arrow Up", "Arrow Down"]],
+	players: Map<string, number> = new Map();
+	private keys: Map<number, string[]> = new Map([
+		[0, ["ArrowUp", "ArrowDown"]],
 		[1, ["w", "s"]],
-		[2, ["Arrow Left", "Arrow Right"]],
+		[2, ["ArrowLeft", "ArrowRight"]],
 		[3, ["a", "d"]],
 	]);
 	
 	// records a connected client
 	handleConnection(client: Socket): void {
-		// disconnect client if there are already 2 connections
-		if (this.connections.size == 2) {
-			console.log(`denied connection from ${client.id}`);
-			client.disconnect();
-		} else {
-			// record player and send back the player number
-			let n: number
-			if (this.connections.size == 0)
-				n = 0;
-			else {
-				for (const player of this.connections.entries())
-				{
-					n = (player[1] + 1) % 2;
-					break ;
-				}
+		if (this.players.size >= 2)
+			return ;
+		// record player and send back the player number
+		let n: number
+		if (this.players.size == 0)
+			n = 0;
+		else {
+			for (const player of this.players.entries())
+			{
+				n = (player[1] + 1) % 2;
+				break ;
 			}
-			this.connections.set(client.id, n);
-			console.log(`user ${client.id} connected`);
-			client.emit('identity', n);
 		}
+		this.players.set(client.id, n);
+		console.log(`user ${client.id} connected`);
+		client.emit('identity', n);
 	}
 	
-	// records an unconnected client
+	// records an unconnected player
 	handleDisconnect(client: Socket): void {
-		console.log(`user ${this.connections.get(client.id)} disconnected`);
-		this.connections.delete(client.id);
+		const player = this.players.get(client.id);
+		if (player === undefined)
+			return ;
+		console.log(`player ${player} disconnected`);
+		// Assume that the disconnected player no longer presses the key
+		for (const k of this.keys.get(player))
+			KeyPressMonitor.delete(k);
+		this.players.delete(client.id);
 	}
 	
-	// records a keydown event
+	// records a keydown event from players only
 	@SubscribeMessage('keydown')
-	keyDown(@MessageBody() data: string, @ConnectedSocket() client: Socket)
+	keyDown(@MessageBody() key: string, @ConnectedSocket() client: Socket)
 			: void {
-		console.log(`Received keydown ${data} from ${client.id}`);
-		KeyPressMonitor.add(data);
+		const player = this.players.get(client.id);
+		if (player === undefined || this.keys.get(player).indexOf(key) < 0) {
+			return ;
+		}
+		console.log(`Received keydown ${key} from ${client.id}`);
+		KeyPressMonitor.add(key);
 	}
 	
-	// records a keyup event
+	// records a keyup event from players only
 	@SubscribeMessage('keyup')
-	keyUp(@MessageBody() data: string, @ConnectedSocket() client: Socket)
+	keyUp(@MessageBody() key: string, @ConnectedSocket() client: Socket)
 			: void {
-		console.log(`Received keyup ${data} from ${client.id}`);
-		KeyPressMonitor.delete(data);
+		const player = this.players.get(client.id);
+		if (player === undefined || this.keys.get(player).indexOf(key) < 0)
+			return ;
+		console.log(`Received keyup ${key} from ${client.id}`);
+		KeyPressMonitor.delete(key);
 	}
 }
