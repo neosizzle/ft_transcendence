aaae365369d0882911a138b888058f0a9f17c987
Ng Wee Hean <heanconcerto@gmail.com>
2022-08-01 13:23:41 +0800

    Refactored frontend Game class

--- frontend/src/Game/index.tsx
+++ frontend/src/Game/index.tsx
@@ -5,39 +5,47 @@ import Canvas from './Canvas';
 import KeyPressMonitor from '../common/game/KeyPressMonitor';
 
 
+/*
+This class is the GameClient.
+There are possibly two types of clients: player or spectator
+*/
 class Game extends React.Component {
-	player = -1;
-	keypress?: KeyPressMonitor | null = null;
+	player = -1;	// -1 for spectator, 0 and above for player number
+	socket: Socket;
+	keypress: KeyPressMonitor | null = null;
 	
-	render() {
-		const socket: Socket = io("http://localhost:3001/game");  // change for production
+	constructor(props: any) {
+		super(props);
+		this.socket = io("http://localhost:3001/game");  // change for production
+		
 		// 'connect' event is fired upon connection the the Namespace
-		socket.on('connect', () => {
+		this.socket.on('connect', () => {
 			console.log('Connected');
 			// keypress monitor is created if connection is made
-			this.keypress = KeyPressMonitor.get_instance(socket);
+			this.keypress = KeyPressMonitor.get_instance(this.socket);
 		});
 		
-		socket.on('identity', (n: number) => {
+		// receives the player number from server
+		this.socket.on('identity', (n: number) => {
 			console.log(`I am player ${n}`);
 			this.player = n;
 		})
 		
-		socket.on('exception', function(data) {
+		this.socket.on('exception', function(data) {
 			console.log('event', data);
 		});
 		
-		socket.on('disconnect', () => {
+		this.socket.on('disconnect', () => {
 			console.log('Disconnected');
 			
 			// keypress monitor is deleted if connection dropped
-			if (this.keypress != null)
-			{
-				delete this.keypress;
+			if (this.keypress != null) {
 				this.keypress = null;
 			}
 		});
-		
+	}
+	
+	render() {
 		return <Canvas width={400} height={300} style={{border: "1px solid black"}}/>;
 	}
 }
