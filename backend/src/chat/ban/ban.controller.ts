3c5c6c0ead242cc95e7fc7c9ed2ef30176268cc3
PandieYay <babyedison@gmail.com>
2022-08-17 19:13:38 +0800

    Ban now deletes member from DB

--- backend/src/chat/ban/ban.controller.ts
+++ backend/src/chat/ban/ban.controller.ts
@@ -1,4 +1,6 @@
 import { Body, Controller, Get, Post, Query, UseGuards } from "@nestjs/common";
+import { User } from "@prisma/client";
+import { GetUser } from "src/users/auth/decorator";
 import { AuthGuard } from "src/users/auth/guard";
 import { ListQuery } from "src/utils";
 import { banDto } from "./ban.dto";
@@ -16,7 +18,7 @@ export class BanController {
     return this.banService.getBan(query);
   }
   @Post()
-  create(@Body() banDto: banDto): Promise<banDto> {
-    return this.banService.giveBan(banDto);
+  create(@GetUser() user : User, @Body() banDto: banDto): Promise<banDto> {
+    return this.banService.giveBan(user, banDto);
   }
 }
--- backend/src/chat/ban/ban.module.ts
+++ backend/src/chat/ban/ban.module.ts
@@ -2,12 +2,13 @@ import { Module } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PrismaModule } from "src/prisma/prisma.module";
 import { PrismaService } from "src/prisma/prisma.service";
+import { MemberService } from "../member/member.service";
 import { BanController } from "./ban.controller";
 import { BanService } from "./ban.service";
 
 @Module({
   imports: [PrismaModule],
   controllers: [BanController],
-  providers: [PrismaService, ConfigService, BanService],
+  providers: [PrismaService, ConfigService, BanService, MemberService],
 })
 export class BanModule {}
