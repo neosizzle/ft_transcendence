9e06c9d198af9daadef60936e68892cf6be34a81
PandieYay <babyedison@gmail.com>
2022-08-21 19:02:11 +0800

    Chat will now display rooms and formatted a bit

--- frontend/src/Chat/index.tsx
+++ frontend/src/Chat/index.tsx
@@ -1,16 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { API_ROOT } from '../constants';
 import { auth_net_get, auth_net_post } from '../utils';
-import { Message } from "./classes";
+import { Message, Room } from "./classes";
 
 const chatEndpoint = `${API_ROOT}/chat`;
+const roomEndpoint = `${API_ROOT}/rooms`;
 
 function Chat() {
 	const [currentMessage, setCurrentMessage] = useState('');
-	// const [totalChats, setTotalChats] = useState(0); // total chat elemnts
 	const [messages, setMessages] = useState<Message[] | null>(null); // The chat
+	const [rooms, setRooms] = useState<Room[] | null>(null); // total chat elemnts
 
 	useEffect(() => {
+		auth_net_get(
+			`${roomEndpoint}?page=1&pageSize=50`
+		).then((data) => {
+			const myRooms = data.data;
+			const roomsArr: Room[] = [];
+			myRooms.forEach((random: Room) => {
+				roomsArr.push(random);
+			});
+			setRooms(roomsArr);
+		})
+
+		//For setting messages 
 		auth_net_get(
 			`${chatEndpoint}?page=1&pageSize=50&filterOn=roomId&filterBy=1&sortBy=Ascending&sortOn=createdAt`
 		).then((data) => {
@@ -21,6 +34,8 @@ function Chat() {
 			});
 			setMessages(msgsArr);
 		});
+		console.log("Wow this is running a lot");
+		//One problem I realised is, this is running very fast. Sometimes api crashes.
 	});
 
 	const sendMessage = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -45,16 +60,22 @@ function Chat() {
 		setCurrentMessage(event.target.value);
 	};
 	const ClearInput = () => {
-	// 👇️ clear input value
-	setCurrentMessage('');
+		// 👇️ clear input value
+		setCurrentMessage('');
 	};
 
 	return (
 		<>
-			<div>
-				<h1 className='text-5xl' id="MyHeader">Chat</h1>
+			{/* Header */}
+			<h1 className='text-5xl'>Chat</h1>
+			<div className='grid grid-cols-2'>
+				<div className='flex flex-row'>
+					{/* Need to figure out how to get user avatar. If DM, get user.avatar as src string. Else if GC, use default? (Alternatively
+						can use owner picture) */}
+					<div className='basis-1/4'>{rooms?.map(room => <p className='border-2' key={room.id}>{room.roomName}</p>)}</div>
+					<div className='h-96'>{messages?.map(message => <p key={message.id}>{message.message}</p>)}</div>
+				</div>
 			</div>
-			<div className='h-96'>{messages?.map(message => <p key={message.id}>{message.message}</p>)}</div>
 			<form onSubmit={(sendMessage)} className="Chat">
 				<input type="text" className="w-fit mt-10 border-2" placeholder="Say something..." id="message" onChange={handleChange} value={currentMessage} />
 			</form>
