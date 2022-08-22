e72f5e03b839334edc52a046879bee4e243175fe
neosizzle <junhanngd1115@gmail.com>
2022-08-23 05:57:56 +0800

    changed docker files for prod

--- backend/entry.sh
+++ backend/entry.sh
@@ -1,6 +1,6 @@
 bash service/userStatus.sh &
-npm install --save-dev webpack
-npx prisma generate
-npm run build
+npm install --save-dev webpack --legacy-peer-deps  &&
+npx prisma generate &&
+npm run build &&
 npm run start:prod
 # tail -f
